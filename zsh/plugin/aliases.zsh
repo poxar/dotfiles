d588c6a98db9a49a79a83e4d9a829a38f5ed32b7
Philipp Millar <philipp.millar@gmx.de>
2013-02-19 14:38:32 +0100

    cleanup aliases

--- zsh/plugin/aliases.zsh
+++ zsh/plugin/aliases.zsh
@@ -28,9 +28,8 @@ alias tm="tmux new"
 alias ta="tmux attach"
 alias tl="tmux ls"
 
-alias uumount="udiskie-umount 2> /dev/null"
 alias weechat="tmux neww -t 0 -d -n chat weechat-curses"
-alias t="todo.sh"
+alias mutt="tmux neww -t 9 -c ~/down -d -n mail ~/bin/mutt"
 
 # pastebin sprunge.us (<command> | sprunge)
 alias sprunge="curl -F 'sprunge=<-' http://sprunge.us"
