683bdf34bb1929f783fef6ec87ce5fc33fef2146
Philipp Millar <philipp.millar@gmail.com>
2013-04-22 17:35:12 +0200

    use the new zoom function of tmux for mutt+offlineimap

--- zsh/plugin/tmux.zsh
+++ zsh/plugin/tmux.zsh
@@ -5,14 +5,19 @@
 
 if which tmux &>/dev/null; then
 
-    alias tm="tmux new"
-    alias ta="tmux attach"
-    alias tl="tmux ls"
+  alias tm="tmux new"
+  alias ta="tmux attach"
+  alias tl="tmux ls"
 
-    # only alias these in a running tmux session
-    if [[ ! -z $TMUX ]]; then
-        alias weechat="tmux neww -t 0 -d -n chat weechat-curses"
-        alias mail="tmux neww -t 9 -c ~/down -d -n mail mutt"
+  # only alias these in a running tmux session
+  if [[ ! -z $TMUX ]]; then
+    alias weechat="tmux neww -t 0 -d -n chat weechat-curses"
+    if which mutt &>/dev/null && \
+      which imap &>/dev/null
+    then
+      alias mail="tmux neww -t 9 -c ~/down -d -n mail mutt && \
+        tmux split -dl 4 -t mail.1 imap && \
+        tmux resize-pane -Z -t mail.1"
     fi
-
+  fi
 fi
