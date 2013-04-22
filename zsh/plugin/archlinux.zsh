3e220ba725a32e3c8f522cc78c6e1ce4ed1bc64f
Philipp Millar <philipp.millar@gmail.com>
2013-04-22 17:35:56 +0200

    remove paclist

--- zsh/plugin/archlinux.zsh
+++ zsh/plugin/archlinux.zsh
@@ -10,10 +10,6 @@ if [[ -r $(which pacman) ]]; then
     alias reflector="$SUDO reflector -c Germany -l 10 --sort score --save \
         /etc/pacman.d/mirrorlist && cat /etc/pacman.d/mirrorlist"
 
-    # list explicitly installed packages that are not in base or base-devel
-    paclist() { pacman -Qei | awk '/^Name/ { name=$3 } /^Groups/
-        { if ( $3 != "base" && $3 != "base-devel" ) { print name } }' }
-
     # build, install and clean a PKGBUILD
     alias mpkg="makepkg -cis"
 
