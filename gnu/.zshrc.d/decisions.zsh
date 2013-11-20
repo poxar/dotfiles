94e13106143d4fef10c7b52aa98911864d5daded
Philipp Millar <philipp.millar@gmx.de>
2013-11-20 14:25:03 +0100

    shuf is in gnu coreutils

--- gnu/.zshrc.d/decisions.zsh
+++ gnu/.zshrc.d/decisions.zsh
@@ -30,3 +30,6 @@ if which shuf &>/dev/null; then
         fi
     }
 fi
+
+# source: http://www.reddit.com/r/commandline/comments/1kubou/some_help_naming_your_web_site/
+alias sitenamr="grep '[^aeiou]er$' /usr/share/dict/words | shuf -n 1 | sed -r -e 's.er$.r.' -e 's.^(\w).\u\1.'"
