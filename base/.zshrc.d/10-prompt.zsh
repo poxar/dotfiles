e4b3be91f03c2fc7445de05ca1ac13dc4fd2ccbb
Philipp Millar <philipp.millar@gmx.de>
2013-11-22 18:44:31 +0100

    simplify the prompt even more
    Just echo the return value, show the hostname only if ssh-ed into a machine and
    put jobcount and pwd in RPROMPT.

--- base/.zshrc.d/10-prompt.zsh
+++ base/.zshrc.d/10-prompt.zsh
@@ -3,20 +3,21 @@
 # .zsh/10-prompt.zsh
 #
 
-### first line
-PROMPT=" "
-# hostname in white
-PROMPT+="%B%m %b"
-# pwd in blue
-PROMPT+="%B%F{blue}%~%f%b"
-# return code in red if != 0
-PROMPT+="%(?.. [%B%F{red}%?%f%b])"
-# number of running background jobs if >0
-PROMPT+="%1(j. (%F{green}%j%f).)"
-
-### second line
-PROMPT+="
- "
+PROMPT=""
+# hostname in white, only if ssh-ed into a machine
+[[ -n $SSH_CLIENT ]] && PROMPT+="%B%m%b "
 # prompt character; red # if root, white > else
 PROMPT+="%B%0(#.%F{red}#%f.>)%b "
 
+RPROMPT=""
+# number of running background jobs in green if >0
+RPROMPT+="%1(j.[%F{green}%j%f].)"
+# pwd in blue
+RPROMPT+=" %B%F{blue}%~%f%b"
+
+# echo return code in red if !=0
+precmd() {
+  last_cmd=$?
+  [[ $last_cmd -ne 0 ]] && echo -e "\e[01;31m$last_cmd\e[00m"
+  unset last_cmd
+}
