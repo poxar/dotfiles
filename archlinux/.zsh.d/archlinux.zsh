397ad4541fe44d08856b001ccc8217da101b01f9
Philipp Millar <philipp.millar@poxar.de>
2013-12-15 20:53:39 +0100

    better mpkg alias

--- archlinux/.zsh.d/archlinux.zsh
+++ archlinux/.zsh.d/archlinux.zsh
@@ -12,4 +12,4 @@ alias reflector="$SUDO reflector -c Germany -l 10 --sort score --save \
   /etc/pacman.d/mirrorlist && cat /etc/pacman.d/mirrorlist"
 
 # build, install and clean a PKGBUILD
-alias mpkg="makepkg -cis"
+alias mpkg="makepkg -rsiL"
