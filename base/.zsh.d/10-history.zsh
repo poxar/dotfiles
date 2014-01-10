6114d0d6264e2a8040893d1e11d11098966d7a4f
Philipp Millar <philipp.millar@poxar.de>
2014-01-10 18:05:00 +0100

    some history tweaks

--- base/.zsh.d/10-history.zsh
+++ base/.zsh.d/10-history.zsh
@@ -13,7 +13,5 @@ HISTIGNORE="exit"
 setopt share_history        # share the history over multiple instances
 setopt extended_history     # put the time into history
 setopt hist_no_store        # don't save history commands
-setopt hist_reduce_blanks   # delete unneeded blanks
-setopt hist_ignore_all_dups # never duplicate entries
+setopt hist_ignore_all_dups # delete old duplicates
 setopt hist_ignore_space    # ignore commands starting with space for history
-setopt hist_verify          # show history-completed commands before execution
