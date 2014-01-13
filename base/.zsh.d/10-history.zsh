48a37bae56c5cfa45930578ef3ede150a69c2b74
Philipp Millar <philipp.millar@poxar.de>
2014-01-13 23:57:13 +0100

    also save history commands

--- base/.zsh.d/10-history.zsh
+++ base/.zsh.d/10-history.zsh
@@ -12,6 +12,5 @@ HISTIGNORE="exit"
 
 setopt share_history        # share the history over multiple instances
 setopt extended_history     # put the time into history
-setopt hist_no_store        # don't save history commands
 setopt hist_ignore_all_dups # delete old duplicates
 setopt hist_ignore_space    # ignore commands starting with space for history
