f8a02963ccba250b422d6de37b8e4c6ffb9637e3
Philipp Millar <philipp.millar@poxar.de>
2014-02-04 17:21:48 +0100

    remove tmux completion

--- tmux/.zsh.d/tmux.zsh
+++ tmux/.zsh.d/tmux.zsh
@@ -6,24 +6,3 @@
 alias tm="tmux new -s std"
 alias ta="tmux attach"
 alias tl="tmux ls"
-
-
-# complete from current tmux pane
-_tmux_pane_words() {
-  local expl
-  local -a w
-  if [[ -z "$TMUX_PANE" ]]; then
-    _message "not running inside tmux!"
-    return 1
-  fi
-  w=( ${(u)=$(tmux capture-pane \; show-buffer \; delete-buffer)} )
-  _wanted values expl 'words from current tmux pane' compadd -a w
-}
-
-zle -C tmux-pane-words-prefix   complete-word _generic
-zle -C tmux-pane-words-anywhere complete-word _generic
-bindkey '^Si' tmux-pane-words-prefix
-bindkey '^S^I' tmux-pane-words-anywhere
-zstyle ':completion:tmux-pane-words-(prefix|anywhere):*' completer _tmux_pane_words
-zstyle ':completion:tmux-pane-words-(prefix|anywhere):*' ignore-line current
-zstyle ':completion:tmux-pane-words-anywhere:*' matcher-list 'b:=* m:{A-Za-z}={a-zA-Z}'
