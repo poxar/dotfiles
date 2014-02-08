b1082171da3371cab8f6f5106faf4ca9574064b8
Philipp Millar <philipp.millar@poxar.de>
2014-02-08 20:43:26 +0100

    bump ruby gem version

--- ruby/.zsh.d/ruby.zsh
+++ ruby/.zsh.d/ruby.zsh
@@ -3,5 +3,7 @@
 # ~/.zsh/ruby.zsh
 #
 
-export GEM_HOME="$HOME/.gem/ruby/2.0.0"
+export GEM_HOME="$HOME/.gem/ruby/2.1.0"
 export PATH="$HOME/.gem/ruby/2.0.0/bin:$PATH"
+export PATH="$HOME/.gem/ruby/2.1.0/bin:$PATH"
+
