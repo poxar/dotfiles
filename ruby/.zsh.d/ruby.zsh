1ff11b94599b862c0f4af58d33bf11d6b46e9fe9
Philipp Millar <philipp.millar@poxar.de>
2014-02-08 20:41:42 +0100

    Revert "use gem man"
    This reverts commit 03745fb9dee65f8a72b818a500495272cd96d5a5.

--- ruby/.zsh.d/ruby.zsh
+++ ruby/.zsh.d/ruby.zsh
@@ -5,15 +5,3 @@
 
 export GEM_HOME="$HOME/.gem/ruby/2.0.0"
 export PATH="$HOME/.gem/ruby/2.0.0/bin:$PATH"
-
-function man() {
-  env \
-    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-    LESS_TERMCAP_md=$(printf "\e[1;31m") \
-    LESS_TERMCAP_me=$(printf "\e[0m") \
-    LESS_TERMCAP_se=$(printf "\e[0m") \
-    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-    LESS_TERMCAP_ue=$(printf "\e[0m") \
-    LESS_TERMCAP_us=$(printf "\e[1;32m") \
-    gem man -s "$@"
-}
