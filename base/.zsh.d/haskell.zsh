5db4bcf86ecc71e0b65767c12885a71ef2eed2c2
Philipp Millar <philipp.millar@poxar.de>
2014-02-09 13:37:14 +0100

    move all language specific settings to host plugins

--- base/.zsh.d/haskell.zsh
+++
@@ -1,6 +0,0 @@
-
-#
-# ~/.zsh/haskell.zsh
-#
-
-export PATH="$HOME/.cabal/bin:$PATH"
--- caprica/.zsh.d/caprica.zsh
+++ caprica/.zsh.d/caprica.zsh
@@ -3,6 +3,19 @@
 # .zsh.d/caprica.zsh
 #
 
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.cabal/bin:$PATH"
+export PATH="$HOME/.gem/ruby/2.0.0/bin:$PATH"
+export PATH="$HOME/.gem/ruby/2.1.0/bin:$PATH"
+
+export GEM_HOME="$HOME/.gem/ruby/2.0.0"
+
+# python virtualenv
+export WORKON_HOME=~/.virtualenvs
+export PROJECT_HOME=$PROJECTS
+[[ -d $WORKON_HOME ]] && \
+    source `which virtualenvwrapper.sh` &>/dev/null
+
 hash -d data=$HOME/data
 hash -d dokumente=~data/Dokumente
 hash -d dropbox=~data/Dropbox
@@ -16,3 +29,4 @@ function mount-tauron() {
   udevil mount -o credentials=/home/philipp/.smbcred-tauron smb://pmi@tauron/$1
 }
 compctl -k "(pmi public backup download configuration)" mount-tauron
+
