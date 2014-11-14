2e98bf9c00a2b0c18959c7ff02ab042ba464baac
Philipp Millar <philipp.millar@poxar.de>
2014-11-14 11:26:41 +0100

    Add etckeeper plugin
    Just a thin wrapper to make it less painful to use.

--- bin/plugins.zsh
+++ bin/plugins.zsh
@@ -11,21 +11,22 @@ plugins=("base")
 [[ $OSTYPE =~ ".*freebsd.*" ]] && plugins+=("os-freebsd")
 
 # guess if other plugins should be included
-which "pacman"   &>/dev/null && plugins+=("plugin-archlinux")
-which "aptitude" &>/dev/null && plugins+=("plugin-aptitude")
+which "pacman"    &>/dev/null && plugins+=("plugin-archlinux")
+which "aptitude"  &>/dev/null && plugins+=("plugin-aptitude")
 
-which "ack"      &>/dev/null && plugins+=("plugin-ack")
-which "ack-grep" &>/dev/null && plugins+=("plugin-ack")
-which "aunpack"  &>/dev/null && plugins+=("plugin-atool")
-which "git"      &>/dev/null && plugins+=("plugin-git")
-which "hg"       &>/dev/null && plugins+=("plugin-mercurial")
-which "keychain" &>/dev/null && plugins+=("plugin-keychain")
-which "svn"      &>/dev/null && plugins+=("plugin-subversion")
-which "tmux"     &>/dev/null && plugins+=("plugin-tmux")
+which "ack"       &>/dev/null && plugins+=("plugin-ack")
+which "ack-grep"  &>/dev/null && plugins+=("plugin-ack")
+which "aunpack"   &>/dev/null && plugins+=("plugin-atool")
+which "git"       &>/dev/null && plugins+=("plugin-git")
+which "hg"        &>/dev/null && plugins+=("plugin-mercurial")
+which "keychain"  &>/dev/null && plugins+=("plugin-keychain")
+which "svn"       &>/dev/null && plugins+=("plugin-subversion")
+which "tmux"      &>/dev/null && plugins+=("plugin-tmux")
+which "etckeeper" &>/dev/null && plugins+=("plugin-etckeeper")
 
-which Xorg       &>/dev/null && plugins+=("plugin-xorg")
-which openbox    &>/dev/null && plugins+=("plugin-openbox")
-which ratpoison  &>/dev/null && plugins+=("plugin-ratpoison")
+which Xorg        &>/dev/null && plugins+=("plugin-xorg")
+which openbox     &>/dev/null && plugins+=("plugin-openbox")
+which ratpoison   &>/dev/null && plugins+=("plugin-ratpoison")
 
 # remove duplicates
 typeset -U plugins
