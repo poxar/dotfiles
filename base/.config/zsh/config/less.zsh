4777b977ba03f81b05e9a76de329ff6b3e96c008
Philipp Millar <philipp.millar@poxar.de>
2015-03-31 15:00:28 +0200

    Stop highlighting the first new line on scrolling

--- base/.config/zsh/config/less.zsh
+++ base/.config/zsh/config/less.zsh
@@ -4,9 +4,8 @@ export LESSHISTFILE=/dev/null
 # -F quit-if-one-screen
 # -M (very) long prompt
 # -R show color escapes
-# -W highlight the first new line, when scrolling
 # -X don't clear the screen on exit
-export LESS='-F -M -R -W -X'
+export LESS='-F -M -R -X'
 
 # use lesspipe or lesspipe.sh, whatever is available
 if (( $#commands[(i)lesspipe(|.sh)] )); then
