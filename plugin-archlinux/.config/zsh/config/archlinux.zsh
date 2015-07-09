aa472948beff8d34a13358c9ba725dd727216c20
Philipp Millar <philipp.millar@poxar.de>
2015-07-10 00:27:00 +0200

    Add alias for listing huge archlinux packages

--- plugin-archlinux/.config/zsh/config/archlinux.zsh
+++ plugin-archlinux/.config/zsh/config/archlinux.zsh
@@ -12,3 +12,5 @@ export ASPROOT=~/.cache/asp
 # command-not-found hook via pkgfile
 source /usr/share/doc/pkgfile/command-not-found.zsh
 
+# list huge packages
+alias hugepkg='expac -s -H M "%-30n %m" | sort -rhk 2 | head'
