e7003febf71ef19ee3ee9f71dbe96e02ff279e77
Philipp Millar <philipp.millar@poxar.net>
2017-09-20 19:05:23 +0200

    Adjust tmux configuration for new version

--- plugin-tmux/.config/zsh/config/tmux.zsh
+++ plugin-tmux/.config/zsh/config/tmux.zsh
@@ -1,4 +1,11 @@
-alias tm="tmux new -s std"
 alias ta="tmux attach"
 alias tl="tmux ls"
 alias tu="tmux-up"
+
+tm() {
+  if command -v tmux-up >/dev/null; then
+    tmux-up $HOME/.tmux/std.conf
+  else
+    alias tm="tmux new -s std"
+  fi
+}
