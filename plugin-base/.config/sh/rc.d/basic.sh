6b6314e0fa4d2f7fc236a7b47cdadbf44575ef13
Philipp Millar <philipp.millar@bexio.com>
2018-09-11 17:40:55 +0200

    Add generic aliases for date -I

--- plugin-base/.config/sh/rc.d/basic.sh
+++ plugin-base/.config/sh/rc.d/basic.sh
@@ -79,6 +79,11 @@ man() {
 }
 
 
+## date
+alias idate='date +%F'
+alias isdate='date +%FT%T%z'
+
+
 ## gcc
 # colorful gcc
 GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
