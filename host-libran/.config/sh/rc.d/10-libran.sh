abc4b257cf66b5aa3444d63c091376ae301110a0
Philipp Millar <philipp.millar@bexio.com>
2019-06-12 21:37:41 +0200

    Rollup libran changes

--- host-libran/.config/sh/rc.d/10-libran.sh
+++ host-libran/.config/sh/rc.d/10-libran.sh
@@ -11,14 +11,20 @@ meeting() {
   n "Meeting/$(date +%Y-%m-%d) $*.md"
 }
 
+# Use locally install ruby gems
+_pd_addpath "$HOME/.gem/ruby/2.3.0/bin"
+
 # Use rustup-init provided rust binaries
 _pd_addpath "$HOME/.cargo/bin"
-
-# Use brew provided curl if available
-_pd_addpath '/usr/local/opt/curl/bin'
 
 # Use sbt 0.13 series if available
 _pd_addpath '/usr/local/opt/sbt@0.13/bin'
 
 # Use postgres 9.5 series if available
 _pd_addpath '/usr/local/opt/postgresql@9.5/bin'
+
+# Use bloop
+_pd_addpath "$HOME/.bloop"
+
+# Use GNU gettext binaries
+_pd_addpath "/usr/local/opt/gettext/bin"
--- host-libran/.config/sh/rc.d/10-libran.zsh
+++ host-libran/.config/sh/rc.d/10-libran.zsh
@@ -1,1 +1,3 @@
 fpath+=(/usr/local/share/zsh/site-functions)
+fpath+=($HOME/.bloop/zsh)
+source /usr/local/opt/git-extras/share/git-extras/git-extras-completion.zsh
