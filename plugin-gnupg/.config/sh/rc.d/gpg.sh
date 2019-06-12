f3e9db711df5a8c81f30a60d002b4cec1bdb4a02
Philipp Millar <philipp.millar@poxar.net>
2019-06-12 19:09:52 +0200

    Make gnupg a bit more xdg conforming

--- plugin-gnupg/.config/sh/rc.d/gpg.sh
+++ plugin-gnupg/.config/sh/rc.d/gpg.sh
@@ -3,5 +3,8 @@ export GPG_TTY
 
 # Use gpg agent as ssh agent
 unset SSH_AGENT_PID
-SSH_AUTH_SOCK=$(gpgconf --list-dirs | grep agent-ssh-socket | cut -d : -f 2)
+SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 export SSH_AUTH_SOCK
+
+# Use data home for gnupg
+export GNUPGHOME=${XDG_DATA_HOME:-$HOME/.local/share}/gnupg
