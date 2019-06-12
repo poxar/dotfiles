3b0efff98c0728f1f3536e8eab42e49036b3b7f6
Philipp Millar <philipp.millar@bexio.com>
2019-06-12 21:35:39 +0200

    More editors

--- plugin-base/.config/sh/rc.d/10-editor.sh
+++ plugin-base/.config/sh/rc.d/10-editor.sh
@@ -4,8 +4,12 @@
 # configures the default editor
 #
 
-if _pd_check vim; then
+if _pd_check nvim; then
+  EDITOR="nvim"
+elif _pd_check vim; then
   EDITOR="vim"
+elif _pd_check nvi; then
+  EDITOR="nvi"
 else
   EDITOR="vi"
 fi
