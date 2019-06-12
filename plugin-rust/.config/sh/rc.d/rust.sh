56c974051737949fe0e9ff61aac77626fad59fa2
Philipp Millar <philipp.millar@poxar.net>
2019-06-12 19:13:03 +0200

    Only configure rustup if it is installed

--- plugin-rust/.config/sh/rc.d/rust.sh
+++ plugin-rust/.config/sh/rc.d/rust.sh
@@ -1,6 +1,3 @@
-RUSTUP_HOME="$HOME/.local/rustup"
-export RUSTUP_HOME
-
 CARGO_HOME="${XDG_CACHE_HOME:-"$HOME/.cache"}/cargo"
 export CARGO_HOME
 
@@ -8,7 +5,13 @@ CARGO_INSTALL_ROOT="$HOME/.local/cargo"
 export CARGO_INSTALL_ROOT
 _pd_addpath "$HOME/.local/cargo/bin"
 
-rust_default_chain="$(rustup toolchain list | grep \(default\) | awk '{ print $1 }')"
-RUST_SRC_PATH="${RUSTUP_HOME:-"$HOME/.rustup"}/toolchains/$rust_default_chain/lib/rustlib/src/rust/src/"
-export RUST_SRC_PATH
-unset rust_default_chain
+# rustup setup
+if _pd_check rustup; then
+  RUSTUP_HOME="$HOME/.local/rustup"
+  export RUSTUP_HOME
+
+  rust_default_chain="$(rustup toolchain list | grep \(default\) | awk '{ print $1 }')"
+  RUST_SRC_PATH="${RUSTUP_HOME:-"$HOME/.rustup"}/toolchains/$rust_default_chain/lib/rustlib/src/rust/src/"
+  export RUST_SRC_PATH
+  unset rust_default_chain
+fi
