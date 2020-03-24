6a3ad86f77bcb72a53e1a0047d2879edb9900b39
Philipp Millar <philipp.millar@poxar.net>
2020-03-24 11:50:25 +0100

    Use dedicated ssh agent instead of gnupg
    This is the first step in slowly fading out gnupg in favor of simpler
    encryption and signing solutions.

--- base/.config/sh/profile.d/50-gnupg.sh
+++ base/.config/sh/profile.d/50-gnupg.sh
@@ -1,10 +1,5 @@
 GPG_TTY=$(tty)
 export GPG_TTY
 
-# Use gpg agent as ssh agent
-unset SSH_AGENT_PID
-SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-export SSH_AUTH_SOCK
-
 # Use data home for gnupg
 export GNUPGHOME="${XDG_DATA_HOME:-"$HOME/.local/share"}/gnupg"
