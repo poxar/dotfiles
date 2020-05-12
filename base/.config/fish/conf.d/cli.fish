67d51993ddd49b1549059e1c434bed74ab44f9dc
Philipp Millar <philipp.millar@poxar.net>
2020-05-13 01:26:03 +0200

    Unify sudo prefixing functions
    Unify __prepend_sudo and __last_sudo into one function and shortcut.
    When the commandline is empty just entering sudo isn't all that helpful,
    thus when can use this case to instead run the functionality __last_sudo
    provided, so fetching the last history line and prepending sudo (or
    removing it if present).

--- base/.config/fish/conf.d/cli.fish
+++ base/.config/fish/conf.d/cli.fish
@@ -6,6 +6,11 @@ function __prepend_sudo
   end
 
   set -l old (commandline -j)
+
+  if test -z "$old"
+    set old (history -1)
+  end
+
   set -l new old
 
   if string match -r "^[[:space:]]*$sudo.*" "$old" 2>&1 >/dev/null
@@ -18,18 +23,6 @@ function __prepend_sudo
 end
 
 bind \cs __prepend_sudo
-
-# prepends sudo to the last entered command
-function __last_sudo
-  set -l sudo 'sudo'
-  if set -q SUDO
-    set sudo "$SUDO"
-  end
-
-  commandline "$sudo $history[1]"
-end
-
-bind \es __last_sudo
 
 # expands ... to ../..
 function __magic_dot
