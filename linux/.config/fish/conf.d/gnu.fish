b388f94d4869da329ed8bbb0222b754f97d86e57
Philipp Millar <philipp.millar@poxar.net>
2020-05-22 17:10:48 +0200

    Make ports an abbreviation

--- linux/.config/fish/conf.d/gnu.fish
+++ linux/.config/fish/conf.d/gnu.fish
@@ -7,3 +7,10 @@ abbr -ag mv mv -b
 
 abbr -ag df df -hT -x tmpfs -x devtmpfs
 abbr -ag bc bc -ql
+
+if command -q lsof
+  abbr -ag ports "lsof -iTCP -sTCP:LISTEN -P"
+  abbr -ag allports "netstat -tulanp"
+else
+  abbr -ag ports "netstat -tulanp"
+end
