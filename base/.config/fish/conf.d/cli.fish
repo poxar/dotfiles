ffc59c492dbbd2b94becb8ab85a112050f494b93
Philipp Millar <philipp.millar@poxar.net>
2021-03-04 18:57:18 +0100

    Add jump to command position shortcut

--- base/.config/fish/conf.d/cli.fish
+++ base/.config/fish/conf.d/cli.fish
@@ -24,6 +24,24 @@ end
 
 bind \cs __prepend_sudo
 
+# jump after first word (ignoring sudo/doas)
+function __command_position
+  set -l cli (commandline -j)
+  set -l cmd ""
+
+  if string match -qr "^sudo " $cli
+    set cmd (string match -r "^sudo \\w*" $cli)
+  else if string match -qr "^doas " $cli
+    set cmd (string match -r "^doas \\w*" $cli)
+  else
+    set cmd (string match -r "^\\w*" $cli)
+  end
+
+  commandline --cursor (string length $cmd)
+end
+
+bind \ea __command_position
+
 # expands ... to ../..
 function __magic_dot
   if string match "*.." (commandline -jc) 2>&1 >/dev/null
