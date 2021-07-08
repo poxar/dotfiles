8df0389467a9c2b0997b332964c9f685a922568a
Philipp Millar <philipp.millar@poxar.net>
2021-07-08 10:48:16 +0200

    More archlinux abbreviations

--- linux-archlinux/.config/fish/conf.d/archlinux.fish
+++ linux-archlinux/.config/fish/conf.d/archlinux.fish
@@ -1,3 +1,11 @@
 abbr -ag makepkg makepkg -rsi
+abbr -ag mp makepkg -rsi
+
 abbr -ag p sudo pacman
+
 abbr -ag s sudo systemctl
+abbr -ag sr sudo systemctl restart
+abbr -ag st systemctl status
+abbr -ag suu systemctl --user
+abbr -ag sur systemctl --user restart
+abbr -ag sus systemctl --user status
