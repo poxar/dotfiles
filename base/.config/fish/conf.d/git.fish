035132e44cd7ae981cff5ebd28d278715bcea848
Philipp Millar <philipp.millar@poxar.net>
2021-08-23 02:13:23 +0200

    More git abbreviations

--- base/.config/fish/conf.d/git.fish
+++ base/.config/fish/conf.d/git.fish
@@ -7,10 +7,15 @@ abbr -ag gdw git diff --word-diff --color-words
 abbr -ag gdo git diff origin
 abbr -ag gl git l
 abbr -ag gco git checkout
+abbr -ag gbr git branch
 abbr -ag gf git fetch
 abbr -ag gfo git fetch origin
 abbr -ag gr git rebase
 abbr -ag gri git rebase -i
+
+# new style checkout
+abbr -ag gs git switch
+abbr -ag grs git restore
 
 function g \
 	--description "Shorthand for git interaction" \
