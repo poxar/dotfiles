eabaaf8c828fba186ee683136f4a442e3a432f3e
Philipp Millar <philipp.millar@poxar.net>
2022-03-12 20:27:09 +0100

    Clean up git abbrs a little

--- base/.config/fish/conf.d/git.fish
+++ base/.config/fish/conf.d/git.fish
@@ -1,6 +1,6 @@
+abbr -ag g git status
 abbr -ag ga git add
 abbr -ag gc git commit
-abbr -ag gca git commit --amend
 abbr -ag gP git push
 abbr -ag gp git pull
 abbr -ag gd git diff
@@ -12,20 +12,9 @@ abbr -ag gbr git branch
 abbr -ag gf git fetch
 abbr -ag gfo git fetch origin
 abbr -ag gr git rebase
-abbr -ag gri git rebase -i
 
 # new style checkout
 abbr -ag gs git switch
 abbr -ag gsm git switch main
 abbr -ag gsd git switch dev
 abbr -ag grs git restore
-
-function g \
-  --description "Shorthand for git interaction" \
-  --wraps git
-  if test (count $argv) -gt 0
-    git $argv
-  else
-    git status --short
-  end
-end
