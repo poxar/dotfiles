4aa466123b3cb2b70e16730140df56cfb3b0522a
Philipp Millar <philipp.millar@poxar.net>
2022-03-23 21:23:59 +0100

    More tmux abbreviations

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -1,4 +1,6 @@
 if command -q tmux
-  abbr -ag tn tmux new-session -A
+  abbr -ag tn tmux new-session
+  abbr -ag ta tmux new-session -A
+  abbr -ag td tmux new-session -As default
   abbr -ag tp tmuxp load -y .tmuxp.yaml
 end
