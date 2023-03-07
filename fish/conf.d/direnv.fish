274be8389058b447f554646fb762b1a5bd23f355
Philipp Millar <philipp.millar@poxar.net>
2023-03-07 02:28:30 +0100

    Use direnv as simple node version manager

---
+++ fish/conf.d/direnv.fish
@@ -0,0 +1,6 @@
+# Simple node version manager
+# Find your version here: https://nodejs.org/en/download/releases/
+# Download required version and unpack to $HOME/Code/node
+# Add `use node SOMEVERSION` to .envrc
+# Profit :)
+set -x NODE_VERSIONS $HOME/Code/node
