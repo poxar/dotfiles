fefa652b88a3892113fbd6da36c4c3344cd33030
Philipp Millar <philipp.millar@poxar.net>
2023-03-07 02:19:57 +0100

    Use less as manpager again

--- fish/functions/man.fish
+++ fish/functions/man.fish
@@ -4,21 +4,6 @@ function man --wraps=man
   set -l width (tput cols)
 
   test "$width" -gt "$MANWIDTH"; and set width $MANWIDTH
-
-  # use nvim as manpager if available
-  if command -q nvim
-    set -x MANPAGER nvim +Man!
-    command man $argv
-  else
-    env \
-      LESS_TERMCAP_mb=(printf "\\e[1;31m") \
-      LESS_TERMCAP_md=(printf "\\e[1;31m") \
-      LESS_TERMCAP_me=(printf "\\e[0m") \
-      LESS_TERMCAP_se=(printf "\\e[0m") \
-      LESS_TERMCAP_so=(printf "\\e[1;44;33m") \
-      LESS_TERMCAP_ue=(printf "\\e[0m") \
-      LESS_TERMCAP_us=(printf "\\e[1;32m") \
-      MANWIDTH="$width" \
-      man $argv
+  env MANWIDTH="$width" man $argv
   end
 end
