36947c7e2b7257b2c0dc8a6c667bb9feeb5bbca0
Philipp Millar <philipp.millar@poxar.net>
2023-03-10 01:11:57 +0100

    Cleanup dap configuration

--- nvim/plugin/dap.lua
+++ nvim/plugin/dap.lua
@@ -3,18 +3,6 @@ local widgets = require('dap.ui.widgets')
 local telescope = require('telescope')
 telescope.load_extension('dap')
 
-local function nmap(keys, func, desc)
-  if desc then
-    desc = 'DAP: ' .. desc
-  end
-
-  vim.keymap.set('n', keys, func, {
-    desc = desc,
-    noremap = true,
-    silent = true
-  })
-end
-
 dap.adapters.php = {
   type = 'executable',
   command = 'node',
@@ -30,16 +18,28 @@ dap.configurations.php = {
   }
 }
 
+local function nmap(keys, func, desc)
+  if desc then
+    desc = 'DAP: ' .. desc
+  end
+
+  vim.keymap.set('n', keys, func, {
+    desc = desc,
+    noremap = true,
+    silent = true
+  })
+end
+
 local function sidebar()
   local my_sidebar = widgets.sidebar(widgets.scopes)
-  my_sidebar.open()
+  my_sidebar.toggle()
 end
 
 nmap('<leader>db', dap.toggle_breakpoint, 'Toggle [D]ebug [B]reakpoint')
 nmap('<leader>dc', dap.continue, '[D]ebug [C]ontinue')
 nmap('<leader>ds', dap.step_into, '[D]ebug [S]tep into')
-nmap('<leader>do', dap.step_over, '[D]ebug [S]tep over')
-nmap('<leader>dx', dap.step_out, '[D]ebug [S]tep out')
+nmap('<leader>do', dap.step_over, '[D]ebug step [O]ver')
+nmap('<leader>dO', dap.step_out, '[D]ebug step [O]ut')
 nmap('<leader>dl', telescope.extensions.dap.list_breakpoints, '[D]ebug [L]ist breakpoints')
 nmap('<leader>di', function() widgets.centered_float(widgets.scopes) end, '[D]ebug [I]nspect Variables/Scopes')
-nmap('<leader>dt', function() widgets.sidebar(widgets.expression).open() end, '[D]ebug [T]oggle sidebar')
+nmap('<leader>dt', sidebar, '[D]ebug [T]oggle sidebar')
