ecd2345d1d62ef7e18b46caa6d35d93bc2abe715
Philipp Millar <philipp.millar@poxar.net>
2023-10-06 14:53:26 +0200

    Make popup windows consistent

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -23,11 +23,12 @@ vim.diagnostic.config({
     }
 })
 
-
+local _border = "single"
+vim.diagnostic.config { float= { border = _border } }
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-  vim.lsp.handlers.hover, { border = "single" })
+  vim.lsp.handlers.hover, { border = _border })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-  vim.lsp.handlers.signature_help, { border = "single"})
+  vim.lsp.handlers.signature_help, { border = _border})
 
 vim.cmd('sign define DiagnosticSignError text=● texthl=DiagnosticSignError linehl= numhl=')
 vim.cmd('sign define DiagnosticSignWarn text=● texthl=DiagnosticSignWarn linehl= numhl=')
