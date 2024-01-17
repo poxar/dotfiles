efa6286002feba05ad4f3c7500a69d27051dee97
Philipp Millar <philipp.millar@poxar.net>
2024-01-17 00:16:07 +0000

    Only load treesitter on compatible installations

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,18 +1,22 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = {
-    "c",
-    "css",
-    "html",
-    "htmldjango",
-    "javascript",
-    "lua",
-    "query",
-    "rust",
-    "typescript",
-    "vim",
-  },
-  sync_install = false,
-  auto_install = true,
-  ignore_install = {},
-  modules = {},
-}
+if vim.fn.has('nvim-0.9') == 1 then
+  vim.cmd.packadd("nvim-treesitter")
+  vim.cmd.packadd("playground")
+  require('nvim-treesitter.configs').setup {
+    ensure_installed = {
+      "c",
+      "css",
+      "html",
+      "htmldjango",
+      "javascript",
+      "lua",
+      "query",
+      "rust",
+      "typescript",
+      "vim",
+    },
+    sync_install = false,
+    auto_install = true,
+    ignore_install = {},
+    modules = {},
+  }
+end
