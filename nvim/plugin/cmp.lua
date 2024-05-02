fe72501e0e6855f90168403628c5c4b2609a40ff
Philipp Millar <philipp.millar@poxar.net>
2024-05-02 11:54:15 +0200

    neovim: Add nvim-lua completion

--- nvim/plugin/cmp.lua
+++ nvim/plugin/cmp.lua
@@ -34,6 +34,7 @@ cmp.setup({
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
     { name = 'snippy' },
     { name = 'nvim_lsp_signature_help' },
     { name = 'crates' },
