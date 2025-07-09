23a9078e5b201bcc63763b3ef29d88aec83e4bb6
Philipp Millar <philipp.millar@poxar.net>
2025-07-09 06:21:36 +0200

    neovim: Add endwise support to npairs

--- nvim/plugin/autopairs.lua
+++ nvim/plugin/autopairs.lua
@@ -1,1 +1,6 @@
-require('nvim-autopairs').setup {}
+local npairs = require('nvim-autopairs')
+
+npairs.setup {}
+npairs.add_rules(require('nvim-autopairs.rules.endwise-elixir'))
+npairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
+npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
