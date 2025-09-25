0f77567dd3e2e8c4e1ac06724a2e2cfff5a490ff
Philipp Millar <philipp.millar@poxar.net>
2025-09-25 11:45:09 +0200

    neovim: Update plugins

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,5 +1,3 @@
-local nvim_lsp = require('lspconfig')
-
 require('fidget').setup {
   text = { spinner = 'dots' },
   timer = { spinner_rate = 200 },
@@ -29,103 +27,99 @@ vim.diagnostic.config({
   }
 })
 
-local on_attach = function(_, bufnr)
-  local function nmap(keys, func)
-    vim.keymap.set('n', keys, func, {
-      buffer = bufnr,
-      noremap = true,
-      silent = true
-    })
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(ev)
+    local function nmap(keys, func)
+      vim.keymap.set('n', keys, func, {
+        buffer = ev.buf,
+        noremap = true,
+        silent = true
+      })
+    end
+
+    nmap('gd', require('telescope.builtin').lsp_definitions)
+    nmap('gD', vim.lsp.buf.declaration)
+
+    nmap('grr', require('telescope.builtin').lsp_references)
+    nmap('gri', require('telescope.builtin').lsp_implementations)
+    nmap('grt', require('telescope.builtin').lsp_type_definitions)
+
+    nmap('gO', require('telescope.builtin').lsp_document_symbols)
+    nmap('grw', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+
+    nmap('gK', 'K')
   end
-
-  nmap('gd', require('telescope.builtin').lsp_definitions)
-  nmap('gD', vim.lsp.buf.declaration)
-
-  nmap('grr', require('telescope.builtin').lsp_references)
-  nmap('gri', require('telescope.builtin').lsp_implementations)
-  nmap('grt', require('telescope.builtin').lsp_type_definitions)
-
-  nmap('gO', require('telescope.builtin').lsp_document_symbols)
-  nmap('grw', require('telescope.builtin').lsp_dynamic_workspace_symbols)
-
-  nmap('gK', 'K')
-end
+})
 
 -- List of available servers
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
 -- https://github.com/neovim/nvim-lspconfig/wiki/Language-specific-plugins
-local servers = {}
 
 -- c family
 if vim.fn.executable("clangd") == 1 then
-  table.insert(servers, "clangd");
+  vim.lsp.enable("clangd")
 end
 
 -- rust
 if vim.fn.executable("rust-analyzer") == 1 then
-  table.insert(servers, "rust_analyzer");
+  vim.lsp.config("rust_analyzer", { procMacro = { enable = true }})
+  vim.lsp.enable("rust_analyzer")
 end
 
 -- lua
 if vim.fn.executable("lua-language-server") == 1 then
-  table.insert(servers, "lua_ls");
+  vim.lsp.config("lua_ls", {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim', 'require' },
+        },
+      },
+    },
+  })
+  vim.lsp.enable("lua_ls")
 end
 
 -- web technology
 if vim.fn.executable("typescript-language-server") == 1 then
-  table.insert(servers, "ts_ls");
+  vim.lsp.enable("ts_ls");
 end
 if vim.fn.executable("vscode-eslint-language-server") == 1 then
-  table.insert(servers, "eslint");
+  vim.lsp.config("eslint", { format = false })
+  vim.lsp.enable("eslint");
 end
 if vim.fn.executable("vscode-css-language-server") == 1 then
-  table.insert(servers, "cssls")
+  vim.lsp.enable("cssls")
 end
 if vim.fn.executable("vscode-html-language-server") == 1 then
-  table.insert(servers, "html")
+  vim.lsp.enable("html")
 end
 if vim.fn.executable("vscode-json-language-server") == 1 then
-  table.insert(servers, "jsonls")
+  vim.lsp.enable("jsonls")
 end
 
 -- markdown
 if vim.fn.executable("marksman") == 1 then
-  table.insert(servers, "marksman");
+  vim.lsp.enable("marksman")
 end
 
 -- yaml
 if vim.fn.executable("yaml-language-server") == 1 then
-  table.insert(servers, "yamlls")
+  vim.lsp.enable("yamlls")
 end
 
 -- python
 if vim.fn.executable("ruff-lsp") == 1 then
-  table.insert(servers, "ruff");
+  vim.lsp.enable("ruff")
 end
 
 -- sh/bash
 if vim.fn.executable("bash-language-server") == 1 then
-  table.insert(servers, "bashls");
+  vim.lsp.enable("bashls")
 end
 
 -- slint ui framework
 if vim.fn.executable("slint-lsp") == 1 then
-  table.insert(servers, "slint_lsp")
-end
-
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    on_attach = on_attach,
-    settings = {
-      ["rust-analyzer"] = {
-        procMacro = { enable = true },
-      },
-      ["Lua"] = {
-        diagnostics = { globals = { 'vim' } }
-      },
-      ["eslint"] = {
-        format = false,
-      },
-    },
-  }
+  vim.lsp.enable("slint_lsp")
 end
