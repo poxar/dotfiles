6bdedb3eedd4e0c87bfc407422755f0bde09d31f
Philipp Millar <philipp.millar@poxar.net>
2025-10-08 17:24:27 +0200

    systemd: Remove notifications for flatpak updates
    This reverts commit 62d2850f40f0bb7662fa439caed7b9ddb00d1fc4.

--- systemd/bin/flatpak-updates.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-set -l updates (flatpak remote-ls --updates --columns=name | string split \n)
-
-if test -n "$updates"
-  notify-send -u critical "Flatpak updates available" "$(string join \n $updates)"
-end
