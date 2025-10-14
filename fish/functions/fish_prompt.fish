c0448ef266728391c206ee75587c5e7e7b9c01ac
Philipp Millar <philipp.millar@poxar.net>
2025-10-14 22:06:11 +0200

    fish: Bring path back into prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,28 +1,33 @@
 function fish_prompt
   set -l last_status $status
+  set -l prompt_char
 
-  # container ID if in distrobox
-  if test -n "$CONTAINER_ID"
-    echo -n "📦$CONTAINER_ID"
+  # path
+  if test $PWD != $HOME
+    echo -n (basename $PWD)
   end
 
-  # hostname if connected through ssh
+  # indicate if we are in a distrobox
+  if test -n "$CONTAINER_ID"
+    set -a prompt_char '📦'
+  end
+
+  # indicate ssh
   if test -n "$SSH_CLIENT"
-    echo -n "$(hostname -f)"
+    set -a prompt_char '󰖟 '
   end
 
   # the prompt character
-  set -l prompt_char
   if fish_is_root_user
-    set prompt_char ' #'
+    set -a prompt_char '#'
   else
-    set prompt_char ' ➜'
+    set -a prompt_char '➜'
   end
 
   # make the prompt char(s) red if the last command failed
   if test $last_status -eq 0
-    echo -n -s "$prompt_char "
+    echo -n -s " $prompt_char "
   else
-    echo -n -s (set_color --bold brred) "$prompt_char " (set_color normal)
+    echo -n -s (set_color --bold brred) " $prompt_char " (set_color normal)
   end
 end
