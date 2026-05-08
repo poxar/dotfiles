c8e9305ad78af11d1319816aeaec56a2cca70ea4
Philipp Millar <philipp.millar@poxar.net>
2026-05-08 11:43:18 +0200

    fish: Add abbreviations for apt

--- fish/conf.d/debian.fish
+++ fish/conf.d/debian.fish
@@ -2,3 +2,11 @@ if command -q dpkg
   abbr -ag apt-rc "dpkg -l | grep '^rc' | awk '{ print $2 }'"
   abbr -ag purge "sudo apt autoremove --purge (dpkg -l | grep '^rc' | awk '{ print $2 }')"
 end
+
+if command -q apt
+  abbr -ag p sudo apt
+  abbr -ag pi sudo apt install
+  abbr -ag pd sudo apt remove
+  abbr -ag pu sudo apt upgrade -U
+  abbr -ag pq sudo apt search
+end
